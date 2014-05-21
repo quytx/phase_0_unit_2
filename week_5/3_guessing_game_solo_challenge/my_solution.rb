c84d9e08bb7319de638d0ffc05888f32a42e8761
quytx1102 <quytx1102@gmail.com>
2014-05-21 16:48:26 -0500

    update 3_die solo challenge

--- week_5/3_guessing_game_solo_challenge/my_solution.rb
+++ week_5/3_guessing_game_solo_challenge/my_solution.rb
@@ -1,30 +1,51 @@
 # U2.W5: Build a simple guessing game SOLO CHALLENGE
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 2. Pseudocode
 
-# Input:
-# Output:
-# Steps:
+# Input: guessed number
+# Output: :high if higher, :low if lower, :correct if equal
+# Steps: 
+# 1. Define the initialization with 2 instance variables: answer(integer) and solved(boolean)
+# 2. Implement guess method
+# 3. Implement solved? method 
 
 
 # 3. Initial Solution
 
 class GuessingGame
   def initialize(answer)
-    # Your initialization code goes here
+    @answer = answer # store the correct answer for each GuessingGame object
+    @solved = false # to keep track of whether or not the last guess was correct
   end
   
-  # Make sure you define the other required methods, too
+  # guess method return feedback for a guess
+  def guess(guessNum)        
+    if guessNum < @answer
+      @solved = false # If we implement our game in the way that it stops whenever a guess is correct, this line might not be needed
+      return :low
+    elsif guessNum > @answer
+      @solved = false
+      return :high 
+    else
+      @solved = true 
+      return :correct      
+    end
+  end
+  
+  # solved method
+  def solved?
+    return @solved
+  end
 end
 
 
 
 
 # 4. Refactored Solution
-
+# none
 
 
 
