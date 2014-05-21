da509fb999bc88bb0916e9b6e696ea402ec66672
quytx1102 <quytx1102@gmail.com>
2014-05-21 16:08:51 -0500

    update 2_die solution

--- week_5/2_die/my_solution.rb
+++ week_5/2_die/my_solution.rb
@@ -1,25 +1,34 @@
 # U2.W5: Die Class 2: Arbitrary Symbols
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input: a non-empty array of strings 
+# Output: a random string in the collection
 # Steps:
+# 1. Define the initialization, taking an array as input type
+# 2. Define the sides method - returns the array's length
+# 3. Define the rol method - returns a random element in the array
 
 
 # 3. Initial Solution
 
 class Die
   def initialize(labels)
+    if labels.empty?
+      raise ArgumentError.new("no element found!")
+    end
+    @labels = labels
   end
 
   def sides
+    return @labels.length
   end
 
   def roll
+    return @labels.sample
   end
 end
 
@@ -40,3 +49,4 @@ end
 
 
 # 5. Reflection 
+# Array's sample method is used.
