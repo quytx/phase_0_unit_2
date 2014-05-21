8b13c0a6c4f35fb4b56d193dd4700cf5c0ca6f39
quytx1102 <quytx1102@gmail.com>
2014-05-21 15:38:33 -0500

    update my solution

--- week_5/1_die/my_solution.rb
+++ week_5/1_die/my_solution.rb
@@ -1,28 +1,35 @@
 # U2.W5: Die Class 1: Numeric
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input: number of sides
+# Output: number of sides or a random number between 1 and # of sides
 # Steps:
+# 1. Create a Die class with one initialization parameter which is a number > 1
+# 2. Define method sides which returns number of sides
+# 3. Define method roll which returns a random number between 1 and # of sides using Random class
+
 
 
 # 3. Initial Solution
 
 class Die
   def initialize(sides)
-    # code goes here
+    if sides < 1
+      raise ArgumentError.new("Number of sides must be greater than 1")
+    end
+    @sides = sides
   end
   
   def sides
-    # code goes here
+    return @sides 
   end
   
   def roll
-    # code goes here
+    return Random.new.rand(@sides) + 1
   end
 end
 
@@ -42,4 +49,7 @@ end
 
 
 
-# 5. Reflection 
+# 5. Reflection
+# The most important thing I learned in this exercise is that you have to put "@" before any instance variable inside a class
+# At first, I put @ before the variable "sides" in the initialization but not in the method, and the test gave me two errors
+# I guess Ruby only knows a variable is an instance variable that belongs to each instance of a class if we put "@" before that variable 
